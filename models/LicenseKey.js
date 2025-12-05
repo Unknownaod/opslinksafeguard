bca2cfac39759db758cbea80b1a6af0cb650068e
Unknownaod <unknown@dona.pw>
2025-12-05 11:52:26 -0500

    Update LicenseKey.js

--- models/LicenseKey.js
+++ models/LicenseKey.js
@@ -3,6 +3,7 @@ const { Schema, model } = require("mongoose");
 
 const licenseKeySchema = new Schema({
   key: { type: String, required: true, unique: true },
+  paymentId: { type: String, required: true },      // ⬅️ ADD THIS
   plan: { type: String, default: "Premier" },
   active: { type: Boolean, default: false },
   ownerId: { type: String, default: null },
