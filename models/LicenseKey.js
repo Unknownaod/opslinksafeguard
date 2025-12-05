a0ec2dd46e8b069e71dcd9ce271c656d34cc4123
Unknownaod <unknown@dona.pw>
2025-12-05 12:47:33 -0500

    Update LicenseKey.js

--- models/LicenseKey.js
+++ models/LicenseKey.js
@@ -4,9 +4,9 @@ const { Schema } = require("mongoose");
 const licenseKeySchema = new Schema({
   key: { type: String, required: true, unique: true },
   paymentId: { type: String, required: true },
-  paymentIntent.customer // ⬅️ Stripe Customer ID
+  customerId: { type: String, required: true },  // ⬅️ NEW
   plan: { type: String, default: "Premier" },
-  active: { type: Boolean, default: false },
+  active: { type: Boolean, default: true },
   ownerId: { type: String, default: null },
   guildId: { type: String, default: null },
   createdAt: { type: Date, default: Date.now }
